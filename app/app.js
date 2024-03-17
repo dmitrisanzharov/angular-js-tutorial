f2f6b72092a092e03fc7bdc44698d7dabf4fc3cd
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-17 17:59:05 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -1,11 +1,30 @@
-angular
-	.module("myMainWrapper", [])
-	.controller("myMainWrapperController", ["$scope", function ($scope) {
-        $scope.message = "Hello World";
+const myModule = angular.module("myMainWrapper", []);
 
-        $scope.changeMessage = function () {
-            console.log('has been clicked');
-            console.log('my scope', $scope);
-            $scope.message = $scope.message === "Hello World" ? "Goodbye World" : "Hello World";
-        };
-    }]);
+myModule.controller("myMainWrapperController", [
+	"$scope",
+	function ($scope) {
+		$scope.message = "Hello World";
+		$scope.myArr2 = ["a", "b", "c", "d", "e", "f"];
+		$scope.myObj = { name: "dmitri", age: "immortal" };
+
+		$scope.changeMessage = function () {
+			console.log("has been clicked");
+			console.log("my scope", $scope);
+			$scope.message =
+				$scope.message === "Hello World"
+					? "Goodbye World"
+					: "Hello World";
+		};
+
+		$scope.changeArray = function () {
+			$scope.myArr = [...$scope.myArr, 4];
+		};
+	},
+]);
+
+myModule.controller("MyController2", [
+	"$scope",
+	function ($scope) {
+		$scope.myVar2 = "hello 2";
+	},
+]);
