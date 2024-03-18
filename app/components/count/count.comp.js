6350c024af4e89502cdf88806bee819b065ecb95
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-18 18:58:59 +0000

    all good 2

--- app/components/count/count.comp.js
+++ app/components/count/count.comp.js
@@ -1,11 +1,17 @@
-angular.module("myMainWrapper", []).directive('countComponent', [function(){
-    return {
-        scope: {
-
+angular.module("myMainWrapper")
+    .component("countComponent", {
+        templateUrl: "app/components/count/count.tpl.html",
+        controllerAs: 'countCtrl',
+        bindings: {
+            title: '='
         },
-        templateUrl: 'app/components/count/count.tpl.html',
-        controller: function($scope){
-            $scope.omg = 'omg did this work';
-        }
+        controller: MyController // OR [MyController]
+    });
+
+function MyController(){
+    this.count = 0;
+    this.incCount = function(){
+        this.count++;
     }
-}])
+    console.log('this', this); 
+}
