91537cbd2d9a10daf14a4cbcf0be5e7b083b6ed8
Victor Sarov <victor.sarov@rentalmatics.com>
2024-03-19 13:35:41 +0000

    all working

--- app/components/count/count.component.js
+++ app/components/count/count.component.js
@@ -2,9 +2,12 @@ angular.module('myMainWrapper').component('countComponent', {
     templateUrl: 'app/components/count/count.tpl.html',
     bindings: {
         countProp: '=',
-        countIncFn: '&'
+        countIncFn: '&?'
     }, 
     controller: function CountController() {
         this.title = 'This is my count component!';
+        let test = Boolean(this.countIncFn);
+        console.log("test: ", test);
+
     }
 })
