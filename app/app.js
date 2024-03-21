79d5b979341ef54fa871d3bbc9b5221ba933ac7b
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-21 20:18:08 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
-const myMainWrapperConst = angular.module("myMainWrapper", []).controller("MainControllerOne", [
+const myMainWrapperConst = angular.module("myMainWrapper", []);
+
+myMainWrapperConst.controller("MainControllerOne", [
 	"$scope",
 	function ($scope) {
 		$scope.var1 = "var1";
+		$scope.myColor = 'red';
 	},
 ]).controller("MainControllerTwo", [
 	"$scope",
 	function ($scope) {
+		console.log('controller2');
 		$scope.var2 = "var2";
 	},
 ]);
