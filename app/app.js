82dd1128b77a8c2447cdf897f4135d13c4bd188a
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-04 20:18:07 +0100

    done, all good

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-console.log('app.js is blank');
+const myMainWrapperConst = angular.module("myMainWrapper", []);
+
+myMainWrapperConst.controller("MyMainWrapperController", [
+	"$scope",
+	function ($scope) {
+		console.log("mainWrapper is working");
+		$scope.test = "test";
+		$scope.myArrMain = ["a", "b", "c", "d", "e", "f"];
+		$scope.myObjMain = { a: "1", b: "2", c: "3", d: "4" };
+
+		$scope.handleChange = function () {
+			console.log("$scope", $scope);
+		};
+	},
+]);
+
+// myMainWrapperConst.controller('MyMainWrapperController2', ['$scope', function($scope){
+//     console.log('mainWrapper is working 2');
+//     $scope.test2 = 'test2';
+// }])
+
+export default myMainWrapperConst;
