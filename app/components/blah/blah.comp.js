e7873301d879b6f93554204f06791f50a60ec88f
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-05 21:59:55 +0100

    all good

--- app/components/blah/blah.comp.js
+++ app/components/blah/blah.comp.js
@@ -3,8 +3,12 @@ import myMainWrapperConst from '/app/app.js';
 myMainWrapperConst.directive('blahComponent', [function(){
     return {
         template: '<h1>Template of Blah</h1>',
+        scope: true,
+        replace: true,
+        restrict: 'E',
         controller: function($scope){
             console.log('scope in Blah', $scope);
+            console.log('parent in Blah', $scope.$parent);
             $scope.blahVar1 = 'var in blah'
         }
     }
