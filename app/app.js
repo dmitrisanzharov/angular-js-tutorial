8926e59d3c217c0cb16cc2690cd67c0b651c15ed
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-07 18:54:50 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,7 +1,28 @@
-const myMainWrapperConst = angular.module('myMainWrapper', []);
+const myMainWrapperConst = angular.module('myMainWrapper', ['ngRoute']);
 
-myMainWrapperConst.controller('MyMainWrapperController', ['$scope', function($scope){
-	// console.log('controller, started: 1')
+myMainWrapperConst.config(['$routeProvider', function($routeProvider) {
+	// routes
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'app/components/views/baz.html'
+		})
+		.when('/index', {
+			templateUrl: 'app/components/views/errorRoute.html',
+			controller: 'ErrorController'
+		})
+		.otherwise({
+			redirectTo: '/index'
+		})
+}])
+
+myMainWrapperConst.controller('MyMainWrapperController', ['$scope', '$rootScope',function($scope, $rootScope){
+	$rootScope.rootVarOne = 'rootVarOne';
+	console.log('root scope in main controller', $rootScope);
+	console.log('scope in main', $scope);
+}]);
+
+myMainWrapperConst.controller('ErrorController', ['$scope', '$rootScope',function($scope, $rootScope){
+	console.log('error controller kicked in');
 }]);
 
 export default myMainWrapperConst;
--- app/components/foo/foo.comp.js
+++ app/components/foo/foo.comp.js
@@ -1,15 +1,22 @@
-import myMainWrapperConst from '/app/app.js';
+import myMainWrapperConst from "/app/app.js";
 
-myMainWrapperConst.directive('fooComponent', [function(){
-    return {
-        templateUrl: 'app/components/foo/foo.temp.html',
-        scope: { },
-        controller: function($scope){
-        // console.log('$scope', $scope)
+myMainWrapperConst.directive("fooComponent", [
+	function () {
+		return {
+			templateUrl: "app/components/foo/foo.temp.html",
+			scope: {},
+			controller: function ($scope, $rootScope) {
+				console.log("$scope in foo", $scope);
+				console.log("$rootScope in foo", $rootScope);
 
-
-            // console.log('foo started');
-            $scope.fooVarOne = 'foo var one';
-        }
-    }
-}]);
+				// console.log('foo started');
+				$scope.fooVarOne = "foo var one";
+				$scope.myArr = [
+					{ name: "a", value: 1 },
+					{ name: "b", value: 2 },
+					{ name: "c", value: 3 },
+				];
+			},
+		};
+	},
+]);
