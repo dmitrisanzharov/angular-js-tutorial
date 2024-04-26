50f1b9d33155e6bbad1c35d094c0fac11ca1280f
Victor Sarov <victor.sarov@rentalmatics.com>
2024-04-26 12:20:42 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-console.log('app.js is blank');
+const myMainWrapperConst = angular.module('myMainWrapper', []);
+
+
+myMainWrapperConst.controller('MyMainWrapperController', ['$scope', function($scope){
+    $scope.myCount1 = true;
+
+
+    $scope.changeCount = function(){
+        console.log('count value', $scope.myCount1);
+        $scope.myCount1 = !$scope.myCount1;
+        console.log('$scope in function', $scope);
+    }
+
+
+    $scope.$watch('myCount1', function(newVal, oldVal, watchScope){
+        console.log('count has changed');
+    })
+
+
+    console.log('$scope end', $scope);
+
+}])
