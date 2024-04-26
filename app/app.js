8881adc17f9c228b0efb906ab11a8c5489abeadd
Victor Sarov <victor.sarov@rentalmatics.com>
2024-04-26 14:08:00 +0100

    all is working

--- app/app.js
+++ app/app.js
@@ -1,20 +1,41 @@
 const myMainWrapperConst = angular.module('myMainWrapper', []);
+let apiUrl = 'http://localhost:5000/dummy'; 
 
 
-myMainWrapperConst.controller('MyMainWrapperController', ['$scope', function($scope){
-    $scope.myCount1 = true;
+myMainWrapperConst.controller('MyMainWrapperController', ['$scope', '$http', '$rootScope',function($scope, $http, $rootScope){
+    $scope.count = 0;
+    $scope.foo = 'foo';
+
+
+    // fails cause no digest cycle
+    fetch(apiUrl)
+    .then(el=> {
+        $scope.count = $scope.count + 1; 
+        $scope.$applyAsync();
+        return el.json()
+    }).then(data => {
+        console.log('data', data);
+    })
+
+    works
+    $http.get(apiUrl).then(el => {
+        $scope.count = $scope.count + 1; 
+        console.log('http', el.data)
+    })
 
 
     $scope.changeCount = function(){
-        console.log('count value', $scope.myCount1);
-        $scope.myCount1 = !$scope.myCount1;
-        console.log('$scope in function', $scope);
+        $scope.count = $scope.count + 1;
     }
 
 
-    $scope.$watch('myCount1', function(newVal, oldVal, watchScope){
-        console.log('count has changed');
-    })
+    $scope.$watch('count', function(newVal, oldVal, watchScope){
+        console.log('watch triggered: count value is: ', $scope.count)
+    });
+
+    // $scope.$watch('foo', function(newVal, oldVal, watchScope){
+    //     // console.log('foo has triggered')
+    // })
 
 
     console.log('$scope end', $scope);
