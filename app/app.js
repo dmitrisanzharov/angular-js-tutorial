6b5acea6feaccc199b1649e5739cb41853525052
fruitloaf <dmitrisanzharov@gmail.com>
2024-05-09 17:49:46 +0100

    all done

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is blank');
+const mainModuleWrapperConst = angular.module('mainModuleWrapper', []); 
+
+mainModuleWrapperConst.controller('MainModuleWrapperController', ['$scope', function($scope){
+    $scope.mainTitle = 'Mah Man Is The Main Title!';
+
+    $scope.$watchCollection('myInput', function(oldVal, newVal, scope){
+        console.log(scope);
+    })
+}]);
+
+export default mainModuleWrapperConst;
