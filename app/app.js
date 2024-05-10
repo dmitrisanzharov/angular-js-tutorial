f47149cb64b1950069bdcc50bf91545a749e91da
Victor Sarov <victor.sarov@rentalmatics.com>
2024-05-10 14:40:34 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -2,10 +2,16 @@ const mainModuleWrapperConst = angular.module('mainModuleWrapper', []);
 
 mainModuleWrapperConst.controller('MainModuleWrapperController', ['$scope', function($scope){
     $scope.mainTitle = 'Mah Man Is The Main Title!';
+    $scope.myArr = ['a', 'b', 'c', 'd', 'e', 'f'];
+    $scope.myObj = {name: 'yo', age: 'omg', blah: 'foo'}
 
     $scope.$watchCollection('myInput', function(oldVal, newVal, scope){
         console.log(scope);
     })
 }]);
+
+mainModuleWrapperConst.controller('MyRandomController', ['$scope', function($scope){
+    $scope.title2 = 'foo title 2 it is';
+}])
 
 export default mainModuleWrapperConst;
