9aaa538916b81a61f672c807e1b4646da11a9c8d
Victor Sarov <victor.sarov@rentalmatics.com>
2024-08-08 14:23:15 +0100

    all done

--- app/services/myServiceThatSaysHello.srv.js
+++ app/services/myServiceThatSaysHello.srv.js
@@ -1,8 +1,7 @@
-
-function myServiceThatSaysHello(){
+function myServiceThatSaysHello(/* do NOT put arguments here, only stuff like: $scope */){
     this.myVar = 'hello from service';
 
-    this.myFnWithArg = function(arg){
+    this.myFnWithArg = function(arg){ /* <-- PUT ARGUMENTS HERE*/
         return 'hello' + arg; 
     }
 }
