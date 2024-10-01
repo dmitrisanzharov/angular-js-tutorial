9e59efb915808aa28f8af4997c9e53984d1aa90a
Victor Sarov <victor.sarov@rentalmatics.com>
2024-10-01 19:59:44 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
 const myAngularAppConst = angular.module('MyAngularAppMainModule', ['ngMaterial', 'ngMessages']);
 
 myAngularAppConst.controller('MyAngularAppMainController', ['$scope', function($scope) {
+    console.log('scope', $scope);
+    $scope.myTitle = 'hello';
 
+    $scope.myArr = [{name: 'one', id: 1}, {name: 'two', id: 2}, {name: 'three', id: 3}];
+
+    $scope.seeScope = function(){
+        console.log('scope in Fn', $scope);
+    }
 }])
