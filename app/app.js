5253d44c8767254123f0a88506c5076690532229
Victor Sarov <victor.sarov@rentalmatics.com>
2024-10-01 19:39:52 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,16 +1,5 @@
-// imports
-import allPagesFn from './pages/indexPagesMain.js';
-import allControllersStandAlone from './controllers/controllersIndex.js';
-import allServices from './services/servicesIndex.js';
-import allComponents from './components/componentsIndex.js';
-import allDirectives from './directives/directivesIndex.js';
-import allFilters from './filters/filtersIndex.js';
+const myAngularAppConst = angular.module('MyAngularAppMainModule', ['ngMaterial', 'ngMessages']);
 
-const myAngularAppConst = angular.module('MyAngularAppMainModule', ['ngRoute', 'ngMaterial', 'ngMessages']);
+myAngularAppConst.controller('MyAngularAppMainController', ['$scope', function($scope) {
 
-allPagesFn(myAngularAppConst); // router basically
-allControllersStandAlone(myAngularAppConst);
-allServices(myAngularAppConst);
-allComponents(myAngularAppConst);
-allDirectives(myAngularAppConst);
-allFilters(myAngularAppConst);
+}])
--- app/controllers/MyAngularAppController.ctrl.js
+++ app/controllers/MyAngularAppController.ctrl.js
@@ -1,13 +1,6 @@
 import { pagesArrayForLinks } from '../pages/indexPagesMain.js';
 
-export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumbersService, $filter){
-    $scope.appTitle = 'My Angular App Navbar';
-    $scope.pagesArray = pagesArrayForLinks;
-    
-    let a = addTwoNumbersService.addThem(1,2);
+export default /*@ngInject*/ function MyAngularAppController (){
 
-    $scope.myArr = [1,2,3];
-
-    let myFilterInsideController = $filter('klToml')(5);
 
 }
