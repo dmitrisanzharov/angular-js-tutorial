ffd52454120b3302f691ad4eea444a41733f4f6c
Victor Sarov <victor.sarov@rentalmatics.com>
2024-10-01 18:54:10 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -8,9 +8,45 @@ import allFilters from './filters/filtersIndex.js';
 
 const myAngularAppConst = angular.module('MyAngularAppMainModule', ['ngRoute', 'ngMaterial', 'ngMessages']);
 
+myAngularAppConst.config(function($mdIconProvider){
+    $mdIconProvider.icon('puppy', 'app/icons/my-icon-doggy.svg');
+    $mdIconProvider.icon('shield', 'app/icons/my-icon-shield.svg')
+})
+
+myAngularAppConst.config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default').primaryPalette('red').accentPalette('lime');
+})
+
+
 allPagesFn(myAngularAppConst); // router basically
-allControllersStandAlone(myAngularAppConst);
+// allControllersStandAlone(myAngularAppConst);
 allServices(myAngularAppConst);
 allComponents(myAngularAppConst);
 allDirectives(myAngularAppConst);
 allFilters(myAngularAppConst);
+
+
+myAngularAppConst.controller('MyAngularAppController', MyAngularAppController);
+
+function MyAngularAppController ($scope, addTwoNumbersService, $filter, $mdMedia){
+    $scope.appTitle = 'Material UI thingy';
+
+    $scope.forLabel = 'for label'
+    
+    let a = addTwoNumbersService.addThem(1,2);
+
+    $scope.myArr = [1,2,3];
+
+    let myFilterInsideController = $filter('klToml')(5);
+
+    $scope.isGreaterThanSmall = $mdMedia('sm');
+    $scope.isGreaterThanSmall = false;
+
+    $scope.trueFalse = true;
+
+    $scope.myFn = function(){
+        console.log('ran')
+        $scope.trueFalse = !$scope.trueFalse;
+        console.log('scope', $scope)
+    }
+}
--- app/controllers/MyAngularAppController.ctrl.js
+++ app/controllers/MyAngularAppController.ctrl.js
@@ -1,7 +1,7 @@
 import { pagesArrayForLinks } from '../pages/indexPagesMain.js';
 
-export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumbersService, $filter){
-    $scope.appTitle = 'My Angular App Navbar';
+export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumbersService, $filter, $mdMedia){
+    $scope.appTitle = 'Material UI thingy';
     $scope.pagesArray = pagesArrayForLinks;
     
     let a = addTwoNumbersService.addThem(1,2);
@@ -10,4 +10,14 @@ export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumb
 
     let myFilterInsideController = $filter('klToml')(5);
 
+    $scope.isGreaterThanSmall = $mdMedia('gt-sm');
+    $scope.isGreaterThanSmall = false;
+
+    $scope.trueFalse = true;
+
+    $scope.myFn = function(){
+        console.log('ran')
+        $scope.trueFalse = !$scope.trueFalse;
+        $scope.$apply();
+    }
 }
