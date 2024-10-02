9b1316a17686b011c64ed00465218de590580826
fruitloaf <dmitrisanzharov@gmail.com>
2024-10-02 20:15:41 +0100

    all done

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
-const myAngularAppConst = angular.module('MyAngularAppMainModule', ['ngMaterial', 'ngMessages']);
+const myAngularAppConst = angular.module("MyAngularAppMainModule", ["ngMaterial", "ngMessages"]);
 
-myAngularAppConst.controller('MyAngularAppMainController', ['$scope', function($scope) {
-    console.log('scope', $scope);
-    $scope.myTitle = 'hello';
+myAngularAppConst.controller("MyAngularAppMainController", [
+	"$scope",
+	function ($scope) {
+		console.log("scope", $scope);
+		$scope.myTitle = "hello";
 
-    $scope.myArr = [{name: 'one', id: 1}, {name: 'two', id: 2}, {name: 'three', id: 3}];
+		$scope.myArr = [
+			{ name: "one", id: 1, other: "omg" },
+			{ name: "two", id: 2, other: "omg2" },
+			{ name: "three", id: 3, other: "omg3" },
+		];
 
-    $scope.seeScope = function(){
-        console.log('scope in Fn', $scope);
-    }
-}])
+		$scope.seeScope = function () {
+			console.log("scope in Fn", $scope);
+		};
+
+		$scope.mySubmit = function () {
+			console.log("submitted");
+		};
+	},
+]);
