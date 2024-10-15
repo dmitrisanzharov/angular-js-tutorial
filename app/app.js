99be16631ca9aac0b4cec8b5f2f2623b3f6bca50
fruitloaf <dmitrisanzharov@gmail.com>
2024-10-15 19:52:07 +0100

    all done

--- app/app.js
+++ app/app.js
@@ -3,4 +3,15 @@ import allControllers from './controllers/index.js'
 
 const myAppWrapperConst = angular.module('myAppWrapperModule', []);
 
+myAppWrapperConst.controller('MyCont1', ['$scope', function($scope) {
+    $scope.foo1 = 'foo1 string';
+    $scope.foo2 = 'foo2 string';
+}])
+
+myAppWrapperConst.controller('MyCont2', ['$scope', function($scope) {
+    $scope.bar1 = 'bar1 lalala';
+    $scope.bar2 = 'bar2 lalala';
+}])
+
+
 allControllers(myAppWrapperConst);
