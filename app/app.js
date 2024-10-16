dd662d81f8a4909ea281d4a8849430b71493ea66
Victor Sarov <victor.sarov@rentalmatics.com>
2024-10-16 18:37:51 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -6,6 +6,18 @@ const myAppWrapperConst = angular.module('myAppWrapperModule', []);
 myAppWrapperConst.controller('MyCont1', ['$scope', function($scope) {
     $scope.foo1 = 'foo1 string';
     $scope.foo2 = 'foo2 string';
+
+    $scope.forInclude = 'is this included?'
+
+    $scope.showScope = function(){
+        console.log('scope', $scope);
+    }
+
+    $scope.changeArr = function(arg){
+        $scope.myVar1.push(arg)
+    }
+
+    $scope.myObj1 = { foo1: 'one', foo2: 'two', foo3: 'three' }
 }])
 
 myAppWrapperConst.controller('MyCont2', ['$scope', function($scope) {
