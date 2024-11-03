1cea3a32f6a8e9ac8c061dcd0f47fef983daf495
Victor Sarov <victor.sarov@rentalmatics.com>
2024-11-03 14:57:26 +0000

    all good

--- app/controllers/mainAppController.ctl.js
+++ app/controllers/mainAppController.ctl.js
@@ -1,25 +1,34 @@
 import { allPagesArray } from '../views/allRoutesFn.js';
 
-export default /*@ngInject*/ function($scope, $filter, myServiceOne){
+export default /*@ngInject*/ function ($scope, $filter, myServiceOne, $location) {
     $scope.allPagesArray = allPagesArray;
     $scope.myAppTitle = 'This Is Victors App';
     $scope.myArr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
-    $scope.myArr2 =  [{name: 'a'}, {name: 'b'}, {name: 'c'}];
-    $scope.myArr3 = ["a", "b", "c", "e", "f", "d"];
+    $scope.myArr2 = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+    $scope.myArr3 = ['a', 'b', 'c', 'e', 'f', 'd'];
 
-    $scope.myObj2 = [{name: 'a', value1: 1, foo: 'b'}, {name: 'b', value1: 2}, {name: 'c', value1: 3}, {name: 'd', value1: 4}];
-    $scope.myArrOfNum = [1,2,3,4,5,6,7,8,9,10];
+    $scope.myObj2 = [
+        { name: 'a', value1: 1, foo: 'b' },
+        { name: 'b', value1: 2 },
+        { name: 'c', value1: 3 },
+        { name: 'd', value1: 4 },
+    ];
+    $scope.myArrOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    let blah = $filter('multiplyByTwo')(5,2,3);
+    let blah = $filter('multiplyByTwo')(5, 2, 3);
     // console.log("blah: ", blah);
 
+    $scope.mySubmit = function () {
+        console.log('submit, scope: ', $scope);
 
-    $scope.mySubmit = function(){
-        console.log('submit, scope: ', $scope)
+        
+    };
+
+    $scope.redirect = function(){
+        console.log('clicked')
+        $location.path('/bar');
     }
-
 
     $scope.sayHello = myServiceOne.sayHello;
     $scope.servVar = myServiceOne.myServiceVar;
-
 }
