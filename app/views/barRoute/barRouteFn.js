8da25ad95604cb393eab68859d489b68e6a759c2
Victor Sarov <victor.sarov@rentalmatics.com>
2024-11-25 17:45:13 +0000

    all good

--- app/views/barRoute/barRouteFn.js
+++ app/views/barRoute/barRouteFn.js
@@ -9,13 +9,22 @@ export default /*@ngInject*/ function ($routeProvider) {
             meta: function(){
                  document.title = 'Bar page'
             }
-        }
+        },
+        reloadOnSearch: false
     });
 }
 
 /*@ngInject*/
-function BarController($scope){
+function BarController($scope, $location){
     console.log('bar controller ran');
+
+    $scope.clickLocation = function(){
+        console.log($location)
+        $location.search({page: '5'});
+        // http://127.0.0.1:5500/index.html#/bar?page=5
+    }
+
+
     $scope.$on('$destroy', function() {
         console.log('destroyed');
     });
