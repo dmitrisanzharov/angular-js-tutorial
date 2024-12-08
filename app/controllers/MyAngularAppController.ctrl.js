bad4ecac008427ddb9e727a19ed0e90dd955d7e3
Victor Sarov <victor.sarov@rentalmatics.com>
2024-12-08 20:19:02 +0000

    dsfsdfsd

--- app/controllers/MyAngularAppController.ctrl.js
+++ app/controllers/MyAngularAppController.ctrl.js
@@ -1,41 +1,25 @@
 import { pagesArrayForLinks } from '../pages/indexPagesMain.js';
 import dataOne from '../data/dataOne.js';
 
-export default /*@ngInject*/ function MyAngularAppController($scope, addTwoNumbersService, $filter) {
+export default /*@ngInject*/ function MyAngularAppController($scope, addTwoNumbersService, $filter, $timeout, $http) {
     $scope.showScope = function(){
         console.log($scope);
     }
-
-    $scope.data = [
-        { id: 1, name: 'Alice' },
-        { id: 2, name: 'Bob' },
-        { id: 3, name: 'Charlie' },
-        { id: 4, name: 'Diana' },
-        { id: 5, name: 'Edward' },
-        { id: 6, name: 'Fiona' },
-        { id: 7, name: 'George' },
-        { id: 8, name: 'Hannah' },
-        { id: 9, name: 'Ian' },
-        { id: 10, name: 'Julia' },
-    ];
-
-    $scope.paginationOptions = {
-        totalItems: $scope.data.length,
-        itemsPerPage: 3,
-        currentPage: 1,
-    };
 
     $scope.gridOptions = {
         data: [],
     };
 
-    $scope.paginate = function () {
-        const start = ($scope.paginationOptions.currentPage - 1) * $scope.paginationOptions.itemsPerPage;
-        const end = start + $scope.paginationOptions.itemsPerPage;
-        $scope.gridOptions.data = $scope.data.slice(start, end);
+    function getServerData(params, callback) {
+        $http.get('https://jsonplaceholder.typicode.com/users').then(function(response) {
+            console.log(response);
+            $scope.gridOptions.data = response.data;
+            $scope.gridOptions.grid.paginationOptions.totalItems = response.data.length;
+            // $scope.gridActions.refresh();
+        })
+    }
 
-        console.log('Page:', $scope.paginationOptions.currentPage, 'Data:', $scope.gridOptions.data);
-    };
+    getServerData();
 
-    $scope.paginate();
+
 }
