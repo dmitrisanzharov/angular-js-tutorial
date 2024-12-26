411c8f399db4a26777e67e258050fe32b27e94a5
fruitloaf <dmitrisanzharov@gmail.com>
2024-12-26 16:19:21 +0000

    all done

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ const myAngularModuleConst = angular.module('myAngularMainModule', []);
 myAngularModuleConst.controller('MyMainController', ['$scope', function($scope) {
     $scope.parentTitle = 'Parent Title String';
     console.log('scope', $scope);
-    $scope.seeScope = function(){
+    $scope.seeScope = function(myArg){
+        // console.log('myArg', myArg);
         console.log($scope);
     }
     $scope.changeArr = function(){
@@ -11,6 +12,10 @@ myAngularModuleConst.controller('MyMainController', ['$scope', function($scope) 
     }
 
     $scope.myObj2 = [{name: 'a', value1: 1, foo: 'b'}, {name: 'b', value1: 2}, {name: 'c', value1: 3}, {name: 'd', value1: 4}];
+
+    $scope.submitForm = function(){
+        console.log('form submitted');
+    }
 }]);
 
 
@@ -18,3 +23,12 @@ myAngularModuleConst.controller('MySecondController', ['$scope', function($scope
     $scope.childTitle = 'Child Title String';
     console.log('scope2', $scope);
 }]);
+
+
+myAngularModuleConst.component('yakComponent', {
+    templateUrl: 'app/html-templates/yak.tpl.html',
+    controller: function YakComponentController(){
+        console.log('yak scope', this);
+        this.yakTitle = 'Yak Title';
+    }
+})
