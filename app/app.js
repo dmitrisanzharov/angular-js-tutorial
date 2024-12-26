dd4b0405668df2915673d42a40416a7bda521873
fruitloaf <dmitrisanzharov@gmail.com>
2024-12-26 11:28:20 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-console.log('app.js is blank');
+const myAngularModuleConst = angular.module('myAngularMainModule', []);
+
+myAngularModuleConst.controller('MyMainController', ['$scope', function($scope) {
+    $scope.parentTitle = 'Parent Title String';
+    console.log('scope', $scope);
+    $scope.seeScope = function(){
+        console.log($scope);
+    }
+    $scope.changeArr = function(){
+        $scope.myArr = ['a', 'b', 'c'];
+    }
+
+    $scope.myObj2 = [{name: 'a', value1: 1, foo: 'b'}, {name: 'b', value1: 2}, {name: 'c', value1: 3}, {name: 'd', value1: 4}];
+}]);
+
+
+myAngularModuleConst.controller('MySecondController', ['$scope', function($scope) {
+    $scope.childTitle = 'Child Title String';
+    console.log('scope2', $scope);
+}]);
