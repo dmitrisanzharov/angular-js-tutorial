61f8d64dc382a067e6ca6d3a07db9b8c52a640a1
Victor Sarov <victor.sarov@rentalmatics.com>
2025-01-23 14:00:39 +0000

    all good

--- app/controllers/MyAngularAppController.ctrl.js
+++ app/controllers/MyAngularAppController.ctrl.js
@@ -1,13 +1,17 @@
 import { pagesArrayForLinks } from '../pages/indexPagesMain.js';
 
-export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumbersService, $filter){
+export default /*@ngInject*/ function MyAngularAppController ($scope, addTwoNumbersService, $filter, $mdMedia){
     $scope.appTitle = 'My Angular App Navbar';
     $scope.pagesArray = pagesArrayForLinks;
     
     let a = addTwoNumbersService.addThem(1,2);
 
-    $scope.myArr = [1,2,3];
+    $scope.myArr = [1,2,3,4,5,6,7];
 
     let myFilterInsideController = $filter('klToml')(5);
+
+    $scope.myVarOne = 6;
+
+    $scope.isGreaterThanSmall = $mdMedia('gt-sm');
 
 }
