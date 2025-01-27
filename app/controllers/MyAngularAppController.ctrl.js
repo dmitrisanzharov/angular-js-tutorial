6c00158c849b62e9e4f716c8424cf3cd2b05ecfc
fruitloaf <dmitrisanzharov@gmail.com>
2025-01-27 16:47:16 +0000

    all good

--- app/controllers/MyAngularAppController.ctrl.js
+++ app/controllers/MyAngularAppController.ctrl.js
@@ -1,30 +1,8 @@
 import { pagesArrayForLinks } from '../pages/indexPagesMain.js';
 import dataOne from '../data/dataOne.js';
 
-export default /*@ngInject*/ function MyAngularAppController($scope, addTwoNumbersService, $filter, $timeout, $http) {
-    $scope.showScope = function(){
-        console.log($scope);
-    }
+export default /*@ngInject*/ function MyAngularAppController($scope) {
 
-    $scope.gridOptions = {
-        data: dataOne,
-        customFilters: {
-            findName: function (items, value, filterName) {
-                console.log(items, value, filterName);
-                if(!value){
-                    return items;
-                }
-                return items.filter(function (item) {
-                    return item.first_name.includes(value);
-                });
-            }
-        }
-    };
 
-    console.log('gridOptions', $scope.gridOptions);
-
-    $timeout(()=> {
-        console.log('gridActions', $scope.gridActions); 
-    }, 0)
 
 }
