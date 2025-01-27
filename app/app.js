2c3af98cd0b241f116d2985d003c1b9073a498f4
fruitloaf <dmitrisanzharov@gmail.com>
2025-01-27 20:51:33 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ import allComponents from "./components/componentsIndex.js";
 import allDirectives from "./directives/directivesIndex.js";
 import allFilters from "./filters/filtersIndex.js";
 
-const dataDummy = [{first_name: 'Dimi', id: 1, last_name: 'CEO'}, {first_name: 'Bob', id: 2, last_name: 'CTO'}, {first_name: 'John', id: 3, last_name: 'CFO'}];
+const dataDummy = [{first_name: 'Dimi', id: 1, last_name: 'Smith'}, {first_name: 'Bob', id: 2, last_name: 'Connoly'}, {first_name: 'John', id: 3, last_name: 'Stewart'}];
 
 const myAngularAppConst = angular.module("MyAngularAppMainModule", ["ngRoute", "ngMaterial", "ngMessages", "dataGrid", "pagination"]);
 
@@ -22,11 +22,36 @@ myAngularAppConst.controller('MyControllerOne', ['$scope', '$timeout',function (
 	$scope.myArrOne = [1,2,3,4,5,6,7,8,9,10];
 
 	$scope.gridOptions = {
-		data: dataDummy
+		data: dataDummy,
+		sort: {
+			predicate: 'id',
+			direction: 'desc'
+		},
+		customFilters: {
+			anyName: function(itemsFromData, valueFromInputField, filterNameFromGridOptionsObj){
+
+				console.log('============================');
+				console.log('itemsFromData', itemsFromData);
+				console.log('valueFromInputField', valueFromInputField);
+
+
+				if(!valueFromInputField){
+					return itemsFromData
+				}
+
+				let final = itemsFromData.filter(function(item){
+					console.log('item', item.last_name)
+					return item.last_name.includes(valueFromInputField)
+				});
+				console.log('final', final)
+
+				return final;
+			}
+		}
 	}
 
 	$timeout(function () {
 		console.log('gridOptions', $scope.gridOptions)
-	}, 0)
+	}, 0);
 	
 }])
