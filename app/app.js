7dd525814ff757f8eabec2b6fab0e0bf522524d5
fruitloaf <dmitrisanzharov@gmail.com>
2025-01-28 19:06:44 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -15,3 +15,11 @@ allComponents(myAngularAppConst);
 allDirectives(myAngularAppConst);
 allFilters(myAngularAppConst);
 
+myAngularAppConst.controller('MyControllerOne', ['$scope', function ($scope) {
+    $scope.myArr = [{name: 'one', id: 1, other: 'omg'}, {name: 'two', id: 2, other: 'omg2'}, {name: 'three', id: 3, other: 'omg3'}];
+
+    $scope.seeScope = function(){
+        console.log($scope);
+    }
+
+}])
