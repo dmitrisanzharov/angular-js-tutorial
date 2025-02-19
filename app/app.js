ff774f627f7d496d84e468323642a661e550feac
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-19 13:26:00 +0000

    all good

--- app/app.js
+++ app/app.js
@@ -6,7 +6,11 @@ import allComponents from "./components/componentsIndex.js";
 import allDirectives from "./directives/directivesIndex.js";
 import allFilters from "./filters/filtersIndex.js";
 
-const dataDummy = [{first_name: 'Dimi', id: 1, last_name: 'Smith'}, {first_name: 'Bob', id: 2, last_name: 'Connoly'}, {first_name: 'John', id: 3, last_name: 'Stewart'}];
+const dataDummy = [
+	{ first_name: "Dimi", id: 1, last_name: "Smith" },
+	{ first_name: "Bob", id: 2, last_name: "Connoly" },
+	{ first_name: "John", id: 3, last_name: "Stewart" },
+];
 
 const myAngularAppConst = angular.module("MyAngularAppMainModule", ["ngRoute", "ngMaterial", "ngMessages", "dataGrid", "pagination"]);
 
@@ -16,3 +20,4 @@ allServices(myAngularAppConst);
 allComponents(myAngularAppConst);
 allDirectives(myAngularAppConst);
 allFilters(myAngularAppConst);
+
