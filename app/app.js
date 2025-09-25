3d99b39e7e6f899fe746859460f90c6cfead63f0
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-25 12:33:54 +0100

    that is strange

--- app/app.js
+++ app/app.js
@@ -2,4 +2,10 @@ const myMainAppWrapperConst = angular.module('myMainAppWrapper', []);
 
 myMainAppWrapperConst.controller('MyMainController', ['$scope', function ($scope) {
     $scope.myMainTitle = 'Hello Foo, this is a String!'
+    $scope.title3 = 'title3 in controller 1'
+}]);
+
+myMainAppWrapperConst.controller('MyMainController2', ['$scope', function ($scope) {
+    $scope.myMainTitle2 = 'Hello Foo 2, this is a String!';
+    $scope.title3 = 'title3 in controller 2'
 }])
