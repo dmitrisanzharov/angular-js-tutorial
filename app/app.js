d43877e30e7673962026e60c9e7a18e512b79d7b
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-25 13:45:02 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
 const myMainAppWrapperConst = angular.module('myMainAppWrapper', []);
 
 myMainAppWrapperConst.controller('MyMainController', ['$scope', function ($scope) {
-    $scope.myMainTitle = 'Hello Foo, this is a String!'
-    $scope.title3 = 'title3 in controller 1'
+    $scope.title3 = 'title from parent'
 }]);
 
 myMainAppWrapperConst.controller('MyMainController2', ['$scope', function ($scope) {
-    $scope.myMainTitle2 = 'Hello Foo 2, this is a String!';
-    $scope.title3 = 'title3 in controller 2'
+
+    console.log('scope in child', $scope)
 }])
