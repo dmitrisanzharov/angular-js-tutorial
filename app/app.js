9fd41cc4c9ac2c659c3135712f928a8a427c49c5
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-02 11:18:41 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -6,6 +6,12 @@ myMainAppWrapperConst.controller('MyMainController', [
         $scope.title3 = 'title from parent';
         $scope.items = [{name: 'name1', value: 1, url: 'https://ih1.redbubble.net/image.1442416307.4191/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'}, {name: 'name2', value: 2}, {name: 'name3', value: 3}];
         $scope.myObj = { foo: 'fooStr', bar: 'barStr', xyx: 'xyxStr', anyKey: 'omg' };
+        $scope.countMain = 0;
+
+
+        $scope.changeCount = function (){
+            $scope.countMain = $scope.countMain + 1;
+        }
 
         $scope.mySubmit = function () {
             console.log('submit');
@@ -25,8 +31,10 @@ angular.module('mySecondModule', []).component('myFirstComponent', {
         console.log('scope?', this);
     },
     bindings: {
-        theName: '='
+        theName: '=',
+        countFromMain: '=',
+        fn: '&'
     },
-    controllerAs: '$alt'
+    controllerAs: '$alt',
 });
 
