7008e5f772c3f50d5e6f10192cbeb3bce35315f4
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-05 10:06:45 +0100

    all working

--- app/app.js
+++ app/app.js
@@ -25,6 +25,7 @@ myMainAppWrapperConst.controller('MyMainController', [
     function ($scope) {
 
     $scope.parentStr = 'parentStr';
+    $scope.colorRed = 'red';
      
        
         // $scope.items = [{name: 'name1', value: 1, url: 'https://ih1.redbubble.net/image.1442416307.4191/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'}, {name: 'name2', value: 2}, {name: 'name3', value: 3}];
