d350d9d3a87357cf4ae33099215ab5ea8982411d
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-07 13:32:33 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,43 +1,55 @@
-export const myMainAppWrapperConst = angular.module('myMainAppWrapper', ['module2']).controller('DummyController', function ($scope) {
-    $scope.dummy1 = 'dummy1';
-}).controller('DummyController2', function ($scope) {
-    $scope.dummy2 = 'dummy2';
-}).component('myFirstComponent', {
-    templateUrl: 'app/myFirstComponentHTML.tpl.html',
-    controller: function MyFirstComponentFn () {
-        this.omgNew = 'omg from component';
-    },
-    bindings: {
-        theName: '=',
-        countFromMain: '=',
-        fn: '&'
-    },
-    controllerAs: '$alt',
-});;
-
-
-
-
-
+export const myMainAppWrapperConst = angular
+    .module('myMainAppWrapper', ['module2'])
+    .controller('DummyController', function ($scope) {
+        $scope.dummy1 = 'dummy1';
+    })
+    .controller('DummyController2', function ($scope) {
+        $scope.dummy2 = 'dummy2';
+    })
+    .component('myFirstComponent', {
+        templateUrl: 'app/myFirstComponentHTML.tpl.html',
+        controller: function MyFirstComponentFn() {
+            this.omgNew = 'omg from component';
+        },
+        bindings: {
+            theName: '=',
+            countFromMain: '=',
+            fn: '&'
+        },
+        controllerAs: '$alt'
+    });
 
 myMainAppWrapperConst.controller('MyMainController', [
     '$scope',
-    function ($scope) {
+    '$rootScope',
+    function ($scope, $rootScope) {
+        $scope.parentStr = 'parentStr';
+        $scope.colorRed = 'red';
 
-    $scope.parentStr = 'parentStr';
-    $scope.colorRed = 'red';
-     
-       
-        // $scope.items = [{name: 'name1', value: 1, url: 'https://ih1.redbubble.net/image.1442416307.4191/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'}, {name: 'name2', value: 2}, {name: 'name3', value: 3}];
+        $scope.dataDummy = [
+            { first_name: 'Dimi', id: 1, last_name: 'Smith' },
+            { first_name: 'Dimi', id: 2, last_name: 'Connoly' },
+            { first_name: 'John', id: 3, last_name: 'Stewart' }
+        ];
+
+        $scope.items = [
+            { name: 'name1', value: 1 },
+            { name: 'name2', value: 2 },
+            { name: 'name3', value: 3 }
+        ];
+
+        $scope.myArr2 = ["a", "b", "c", "e", "f", "d"]; 
         // $scope.myObj = { foo: 'fooStr', bar: 'barStr', xyx: 'xyxStr', anyKey: 'omg' };
         $scope.countMain = 0;
 
-        // console.log('MyMainController', $scope);
+        console.log('MyMainController', $scope);
+        console.log('MyMainController root', $rootScope);
 
+        $rootScope.fooBar = 'fooBarRoot';
 
-        $scope.changeCount = function (){
+        $scope.changeCount = function () {
             $scope.countMain = $scope.countMain + 1;
-        }
+        };
 
         // $scope.mySubmit = function () {
         //     console.log('submit');
@@ -49,32 +61,30 @@ myMainAppWrapperConst.controller('MyMainController', [
     }
 ]);
 
-myMainAppWrapperConst.directive('customDirective', function(){
+myMainAppWrapperConst.directive('customDirective', function () {
     return {
         template: '<div>custom directive: {{$parent.parentStr}}</div><ng-transclude />',
         scope: {
-            myPropOne: '=',
+            myPropOne: '='
         },
-        controller: function($scope) {
+        controller: function ($scope, $rootScope) {
             console.log('child', $scope);
             $scope.dirVar = 'dirVariable';
+            delete $rootScope.fooBar;
         },
         transclude: true,
         restrict: 'E'
-    }
+    };
 });
 
-myMainAppWrapperConst.config(function(){
+myMainAppWrapperConst.config(function () {
     console.log('myMainAppWrapperConst config');
-})
+});
 
-myMainAppWrapperConst.run(function(){
+myMainAppWrapperConst.run(function () {
     console.log('run config');
-})
+});
 
 myMainAppWrapperConst.controller('MyDummyController', function ($scope) {
     $scope.dummyVar = 'dummyVar';
-});
-
-myNinjaAppConst.controller('NinjaControllerAsName', /* @ngInject */ function($scope, otherDependencies){ // $scope as a dependency injection
 });
