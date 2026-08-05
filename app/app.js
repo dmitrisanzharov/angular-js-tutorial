558af62c963cd6ff1284d0be604fbe6a3ab4b04f
VictorSarov <victor.sarov@rentalmatics.com>
2026-08-05 15:31:10 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const appModuleConst = angular.module('myMainModuleString', []);
 
 console.log('appModuleConst', appModuleConst);
 
-appModuleConst.controller('MyMainController', ['$scope', function($scope){
+angular.module('myMainModuleString').controller('MyMainController', ['$scope', function($scope){
     $scope.sayHi = 'hello from controller';
 
     $scope.count = 0;
@@ -13,6 +13,10 @@ appModuleConst.controller('MyMainController', ['$scope', function($scope){
     $scope.add = function(){
         $scope.count = $scope.count + 1
         console.log('scope in count', $scope);
+    }
+
+    $scope.myFnReturn = function(){
+        return 5;
     }
 
     console.log('scope', $scope);
