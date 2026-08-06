41b7ab061749058a07ac124ec23f8176dbdca939
VictorSarov <victor.sarov@rentalmatics.com>
2026-08-06 07:46:33 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,23 +1,29 @@
-console.log('app started');
+// console.log('app started');
 
 const appModuleConst = angular.module('myMainModuleString', []);
 
 console.log('appModuleConst', appModuleConst);
 
-angular.module('myMainModuleString').controller('MyMainController', ['$scope', function($scope){
+angular.module('myMainModuleString').controller('MyMainController', /*@ngInject*/ function($scope, $sce){
     $scope.sayHi = 'hello from controller';
 
     $scope.count = 0;
 
+    $scope.myArr = ['a', 'b', 'c'];
+
+    $scope.changeCheckBox = function(){
+         // console.log('scope in change checkbox', $scope);
+    }
+
 
     $scope.add = function(){
         $scope.count = $scope.count + 1
-        console.log('scope in count', $scope);
+        // console.log('scope in count', $scope);
     }
 
     $scope.myFnReturn = function(){
-        return 5;
+        return $sce.trustAsHtml('5');
     }
 
-    console.log('scope', $scope);
-}])
+    // console.log('scope', $scope);
+})
