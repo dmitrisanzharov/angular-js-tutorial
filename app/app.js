ba0b944abcb0a1528c77209fdeb16bf5024d1778
VictorSarov <victor.sarov@rentalmatics.com>
2026-08-09 11:38:18 +0100

    all good

--- app/app.js
+++ app/app.js
@@ -1,22 +1,86 @@
-export const appModuleConst = angular.module('myMainModuleString', ['moduleTwo']);
+export const appModuleConst = angular.module('myMainModuleString', ['moduleTwo', 'ngRoute']);
+
+appModuleConst.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'app/pages/page2/page2.html'
+        })
+        .when('/page1', {
+            templateUrl: 'app/pages/page1/page1.html'
+        })
+        .when('/new', {
+            template: '<h1>this is new page - {{blah}}</h1>',
+            controller: function NewRouteController($scope){
+                $scope.blah = 'blah'
+            }
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
+
+// appModuleConst.filter('arrayHasLetterInIt', function () {
+//     return function (...args) {
+//         console.log('args', args);
+
+//         let firstItemOnTheLeft = args[0];
+//         let argsAreItemsOnTheRightAfterSemiColumn = args[1];
+
+//         return firstItemOnTheLeft.filter((item) => {
+//             console.log('iteration of item', item);
+//             return item.includes(argsAreItemsOnTheRightAfterSemiColumn);
+//         });
+//     };
+// });
+
+// appModuleConst.filter('multiplyBySecond', function () {
+//     return function (...args) {
+//         let arg1 = args[0];
+//         let arg2 = args[1];
+
+//         return arg1 * arg2;
+//     };
+// });
 
 // console.log('appModuleConst', appModuleConst);
 
-appModuleConst.config(function(){
-    console.log('config ran');
-})
+// appModuleConst.config(function(){
+//     console.log('config ran');
+// })
+
+// appModuleConst.run(function(){
+//     console.log('run ran');
+// })
 
 appModuleConst.controller(
     'MyMainController',
-    /*@ngInject*/ function ($scope, $sce) {
+    /*@ngInject*/ function ($scope, $sce, $filter) {
+        $scope.items = [
+            { name: 'name1', value: 1 },
+            { name: 'name2', value: 2 },
+            { name: 'name3', value: 3 }
+        ];
+
+        $scope.myArr2 = ['a', 'b', 'c', 'e', 'f', 'd'];
+
+        $scope.myObj2 = [
+            { name: 'a', value1: 1 },
+            { name: 'b', value1: 2 },
+            { name: 'c', value1: 3 },
+            { name: 'd', value1: 4 }
+        ];
+
+        // let test = $filter('arrayHasLetterInIt')($scope.myArr2, 'b', 'c');
+        // console.log('test', test);
+
         $scope.sayHi = 'hello from main controller';
 
         $scope.count = 0;
 
-        $scope.incCountFn = function(){
+        $scope.incCountFn = function () {
             console.log('incCountFn ran');
             $scope.count = $scope.count + 1;
-        }
+        };
 
         $scope.isDisabled = true;
 
@@ -45,8 +109,6 @@ appModuleConst.controller(
     }
 );
 
-
-
 appModuleConst.component('componentOne', {
     templateUrl: 'app/templates/componentOne.html',
     transclude: true,
@@ -58,23 +120,23 @@ appModuleConst.component('componentOne', {
     controller: ComponentOneController
 });
 
-function ComponentOneController() {
-   
+function ComponentOneController($rootScope) {
+    console.log('componentOneController', $rootScope);
 }
 
-
 // CUSTOM DIRECTIVE
-appModuleConst.directive('directiveOne', function(){
+appModuleConst.directive('directiveOne', function () {
     return {
         template: '<h1>Directive One: {{directiveOneStr}} and prop is: {{myPropOne}} and {{$parent.sayHi}}</h1>',
         scope: {
             myPropOne: '='
         },
-        controller: function($scope){
+        controller: function ($scope, $rootScope) {
             $scope.directiveOneStr = 'directiveOneStr';
-            console.log('scope in directiveOne', $scope)
+            console.log('scope in directiveOne', $scope);
+            console.log('============================');
+            console.log('root in directiveOne', $rootScope);
+            $rootScope.omg = 'omg';
         }
-    }
-})
-
-
+    };
+});
